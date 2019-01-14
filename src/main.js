95cf15e223bfde5b0248bab0b0f31e889ba3a767
chengheai <842385139@qq.com>
2019-01-14 15:56:29 +0800

    🌈init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import _ from 'lodash'
+import VueWeekPicker from './packages/index.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 默认主题
 import 'element-ui/lib/theme-chalk/display.css'
 
 Vue.prototype._ = _
 Vue.use(ElementUI, { size: 'small' })
+Vue.use(VueWeekPicker)
 new Vue({
   el: '#app',
   render: h => h(App)
--- src/packages/index.js
+++ src/packages/index.js
@@ -1,5 +1,7 @@
 import VueWeekPicker from './vue-week-picker.vue';
 
 VueWeekPicker.install = Vue => Vue.component(VueWeekPicker.name, VueWeekPicker);
-
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(VueWeekPicker);
+}
 export default VueWeekPicker;
