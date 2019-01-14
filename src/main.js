f2aa3d9181292cc4ff4cc0d24106dbad56f6af02
chengheai <842385139@qq.com>
2019-01-14 15:29:53 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 import _ from 'lodash'
-Vue.config.productionTip = false
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 默认主题
+import 'element-ui/lib/theme-chalk/display.css'
 
 Vue.prototype._ = _
-
-/* eslint-disable no-new */
+Vue.use(ElementUI, { size: 'small' })
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
---
+++ src/packages/index.js
@@ -0,0 +1,5 @@
+import VueWeekPicker from './vue-week-picker.vue';
+
+VueWeekPicker.install = Vue => Vue.component(VueWeekPicker.name, VueWeekPicker);
+
+export default VueWeekPicker;
